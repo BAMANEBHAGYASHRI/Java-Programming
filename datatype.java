5659c3985ed1af50ae2b78d56df73b223f20143c
BAMANEBHAGYASHRI <shrutibamaneb@gmail.com>
2024-08-06 11:51:24 +0530

    datatypes in java

---
+++ datatype.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class datatype {
+    public static void main(String[] args) {
+        //input
+         Scanner sc= new Scanner(System.in);
+         String name=sc.next();
+        // String name=sc.nextLine();
+        System.out.println(name);
+
+        // pratice code
+        Scanner scc =new Scanner(System.in);
+        int a= scc.nextInt();
+        int b = scc.nextInt();
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
