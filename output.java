abbb4595bb4e2e0cf95bb84e3e5c843277ad8be9
BAMANEBHAGYASHRI <shrutibamaneb@gmail.com>
2024-08-06 11:50:50 +0530

    output function

---
+++ output.java
@@ -0,0 +1,14 @@
+public class output {
+    public static void main(String[] args) {
+        //print the pattern
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        // to print
+
+        
+        System.out.println("*\n**");
+
+    }
+}
