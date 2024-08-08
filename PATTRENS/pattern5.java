1307bd106d580df72a58216f69aeabc282af2a1c
BAMANEBHAGYASHRI <shrutibamaneb@gmail.com>
2024-08-08 12:06:12 +0530

    patterns using nested loop

---
+++ PATTRENS/pattern5.java
@@ -0,0 +1,27 @@
+package PATTRENS;
+public class pattern5 {
+    public static void main(String args[] ) {
+        int n=4;
+
+        for(int i=1; i<=n; i++){
+                // space print
+            for(int j=1; j<=n-i; j++){
+                    System.out.print(" ");
+            }
+            //star print
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+//inverted half pyramid
+/*
+         *
+      *  *
+     * * *
+   * * * *
+ */
